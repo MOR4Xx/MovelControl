a187d9bb80b8a4c46f07ab1b13d8a43d52e8b237
MOR4Xx <jarabelo68@gmail.com>
2025-05-15 20:28:51 -0300

    Commit 15/05 20:28 Jorge

---
+++ src/main/java/com/web2/movelcontrol/Exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.web2.movelcontrol.Exceptions;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
