f5b6c2b7ed69c0933e78a28e0f3533921777a7ac
LDRRosa <leandro.rosa.prof@gmail.com>
2025-05-20 09:33:22 -0300

    inclusão do FornecedorController

--- src/main/java/com/web2/movelcontrol/Controller/fornecedorController.java
+++ src/main/java/com/web2/movelcontrol/Controller/fornecedorController.java
@@ -2,11 +2,15 @@ package com.web2.movelcontrol.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.web2.movelcontrol.Model.Fornecedor;
 import com.web2.movelcontrol.Model.Fornecedor;
 import com.web2.movelcontrol.Service.FornecedorService;
 
@@ -23,5 +27,17 @@ public class fornecedorController {
     public Fornecedor createFornecedor(@RequestBody Fornecedor fornecedor) {
         return service.create(fornecedor);
     }
+
+    @DeleteMapping(value = "/deletar/{id}")
+    public void deleteFornecedor(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+        @PutMapping("/atualizar/{id}")
+    public Fornecedor atualizarFornecedor(@PathVariable Long id, @RequestBody Fornecedor pf){
+        return service.update(id, pf);
+    }
+
+
 
 }
