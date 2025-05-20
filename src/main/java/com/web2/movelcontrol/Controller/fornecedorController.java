1c3ebf96620530a3afae24ef29a738bb2a6d1632
LDRRosa <leandro.rosa.prof@gmail.com>
2025-05-20 08:21:03 -0300

    inclusão do post fornecedor

---
+++ src/main/java/com/web2/movelcontrol/Controller/fornecedorController.java
@@ -0,0 +1,27 @@
+package com.web2.movelcontrol.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.web2.movelcontrol.Model.Fornecedor;
+import com.web2.movelcontrol.Service.FornecedorService;
+
+
+@RestController
+@RequestMapping("/fornecedor")
+public class fornecedorController {
+
+    @Autowired
+    FornecedorService service;
+
+@PostMapping(value = "/criar",
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Fornecedor createFornecedor(@RequestBody Fornecedor fornecedor) {
+        return service.create(fornecedor);
+    }
+
+}
