e7a1c98daaf3e3dca1c6ddf9331fc11ba6ec9931
Jorge <jarabelo68@gmail.com>
2025-06-15 15:50:26 -0300

    Adicionado Cache no Item

--- src/main/java/com/web2/movelcontrol/MovelControlApplication.java
+++ src/main/java/com/web2/movelcontrol/MovelControlApplication.java
@@ -2,8 +2,10 @@ package com.web2.movelcontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MovelControlApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/web2/movelcontrol/Service/ItemService.java
+++ src/main/java/com/web2/movelcontrol/Service/ItemService.java
@@ -3,12 +3,17 @@ package com.web2.movelcontrol.Service;
 import com.web2.movelcontrol.Exceptions.NotFoundException;
 import com.web2.movelcontrol.Model.Item;
 import com.web2.movelcontrol.Repository.ItemRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
 
 @Service
+@CacheConfig(cacheNames = "item")
 public class ItemService {
 
     @Autowired
@@ -16,17 +21,20 @@ public class ItemService {
 
     Logger logger = Logger.getLogger(Item.class.getName());
 
+    @Cacheable(key = "#id")
     public Item create(Item item) {
         logger.info("Item criado com sucesso");
         return repository.save(item);
     }
 
+    @Cacheable(key = "#id")
     public Item findById(Long id) {
         logger.info("Item buscado por ID: " + id);
         return repository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Item não encontrado com ID: " + id));
     }
 
+    @CachePut(key = "#id")
     public Item update(Long id, Item itemNovo) {
         Item itemAntigo = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException("Item não encontrado com ID: " + id));
@@ -39,6 +47,7 @@ public class ItemService {
         return repository.save(itemAntigo);
     }
 
+    @CacheEvict(value = "item", key = "#id")
     public void delete(Long id) {
         repository.deleteById(id);
     }
